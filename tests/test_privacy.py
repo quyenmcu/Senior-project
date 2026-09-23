91312c7b87070e481a6177942780b35673e31ac9
quyenmcu <129649836+quyenmcu@users.noreply.github.com>
2026-09-23 17:35:45 +0800

    Remove remaining duplicated merge fixtures

--- tests/test_privacy.py
+++ tests/test_privacy.py
@@ -8,10 +8,6 @@ from apps.driving.models import DrivingSession, FatigueEvent
 
 @pytest.mark.django_db
 def test_dashboard_does_not_show_another_users_events(client):
-    owner = User.objects.create_user("owner", password="test-password")
-    intruder = User.objects.create_user("intruder", password="test-password")
-    session = DrivingSession.objects.create(user=owner, started_at=timezone.now())
-    FatigueEvent.objects.create(user=owner, session=session, recorded_at=timezone.now(), status="SLEEPING")
     owner = User.objects.create_user("owner")
     intruder = User.objects.create_user("intruder")
     session = DrivingSession.objects.create(user=owner, started_at=timezone.now())
